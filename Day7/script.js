bff64ecd6db76403b8f6ed5f9ce745c58da9c3ee
ShivamM2002 <78159964+ShivamM2002@users.noreply.github.com>
2021-12-23 18:59:47 +0530

    Update script.js

--- Day7/script.js
+++ Day7/script.js
@@ -1,10 +1,11 @@
 setInterval(() => {
   d = new Date();
+
   htime = d.getHours();
   mtime = d.getMinutes();
   stime = d.getSeconds();
-  hrotation = 30 * htime + mtime / 2;
-  mrotation = 6 * mtime;
+  hrotation = 30 * htime + mtime / 2 + stime / 120;
+  mrotation = 6 * mtime + stime / 10;
   srotation = 6 * stime;
   document.querySelector(".hour").style.transform = `rotate(${hrotation}deg)`;
   document.querySelector(".minute").style.transform = `rotate(${mrotation}deg)`;
