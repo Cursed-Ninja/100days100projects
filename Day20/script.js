2d213e5b94f703d4cd030236229b8093e610e658
ShivamM2002 <78159964+ShivamM2002@users.noreply.github.com>
2022-01-01 18:40:59 +0530

    Add files via upload

---
+++ Day20/script.js
@@ -0,0 +1,59 @@
+const dropList = document.querySelectorAll(".drop-list select");
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
+const getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+  for (currency_code in country_list) {
+    let selected;
+    if (i == 0) {
+      selected = currency_code == "USD" ? "selected" : "";
+    } else if (i == 1) {
+      selected = currency_code == "INR" ? "selected" : "";
+    }
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+}
+
+window.addEventListener("load", (e) => {
+  getExchangeRate();
+});
+
+getButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", () => {
+  let tempCode = fromCurrency.value;
+  fromCurrency.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  getExchangeRate();
+});
+
+function getExchangeRate() {
+  const amount = document.querySelector(".amount input");
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+  const exchangeRateTxt = document.querySelector(".exchange-rate");
+
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+  let url = `http://api.exchangeratesapi.io/v1/latest?access_key=${Api_key}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let fromRate = result.rates[`${fromCurrency.value}`];
+      let toRate = result.rates[`${toCurrency.value}`];
+      let exchangeRate = toRate / fromRate;
+      let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    })
+    .catch(() => {
+      exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
