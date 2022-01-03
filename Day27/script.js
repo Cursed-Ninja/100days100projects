c69d14146c2b3a91c777524edaa302705f6203f0
ShivamM2002 <78159964+ShivamM2002@users.noreply.github.com>
2022-01-04 01:04:09 +0530

    Update script.js

--- Day27/script.js
+++ Day27/script.js
@@ -8,12 +8,23 @@ canvas.width = parseInt(document.querySelector("#paint").offsetWidth);
 canvas.height = parseInt(document.querySelector("#paint").offsetHeight);
 
 var mouse = { x: 0, y: 0 };
+var touch = { x: 0, y: 0 };
 
 canvas.addEventListener(
   "mousemove",
   function (e) {
     mouse.x = e.pageX - this.offsetLeft;
     mouse.y = e.pageY - this.offsetTop;
+    console.log(e.pageX);
+  },
+  false
+);
+
+canvas.addEventListener(
+  "touchmove",
+  function (e) {
+    touch.x = e.touches[0].pageX - this.offsetLeft;
+    touch.y = e.touches[0].pageY - this.offsetTop;
   },
   false
 );
@@ -48,7 +59,6 @@ canvas.addEventListener(
   function (e) {
     ctx.beginPath();
     ctx.moveTo(mouse.x, mouse.y);
-
     canvas.addEventListener("mousemove", onPaint, false);
   },
   false
@@ -62,7 +72,42 @@ canvas.addEventListener(
   false
 );
 
+canvas.addEventListener(
+  "touchstart",
+  function (e) {
+    //   console.log("here");
+    // console.log(e.touches[0].clientX);
+    ctx.beginPath();
+    // let rect = canvas.getBoundingClientRect();
+    // let x = e.touches[0].clientX - rect.left;
+    // let y = e.touches[0].clientX - rect.top;
+    ctx.moveTo(touch.x, touch.y);
+    // console.log(Touch.clientX);
+    canvas.addEventListener("touchmove", onPaintTouch, false);
+  },
+  false
+);
+
+canvas.addEventListener(
+  "touchend",
+  function (e) {
+    canvas.removeEventListener("touchmove", onPaintTouch, false);
+  },
+  false
+);
+
 var onPaint = function () {
   ctx.lineTo(mouse.x, mouse.y);
+  ctx.stroke();
+};
+
+var onPaintTouch = function (e) {
+  //   console.log(e);
+  //   let rect = canvas.getBoundingClientRect();
+  //   let x = e.touches[0].clientX - rect.left;
+  //   let y = e.touches[0].clientX - rect.top;
+  //   console.log("here");
+  console.log(touch.x);
+  ctx.lineTo(touch.x, touch.y);
   ctx.stroke();
 };
